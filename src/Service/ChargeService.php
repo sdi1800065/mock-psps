c543fb91c76a2b7aeadb9d73a9754d60441dc965
ioannis <karakolisioannis2000@gmail.com>
2026-03-23 15:16:29 +0200

    implement charge core service

---
+++ src/Service/ChargeService.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MockPsps\Service;
+use MockPsps\Repository\ChargeRepositoryInterface;
+use MockPsps\Psp\PspInterface;
+use MockPsps\Model\Merchant;
+use MockPsps\Model\Charge;
+
+class ChargeService {
+    
+    public function __construct(
+        private ChargeRepositoryInterface $chargeRepository ,
+        private array $psps,
+    ) {}
+
+    public function charge(Merchant $merchant, array $params): Charge
+    {
+        $merchantPsp = $this->psps[$merchant->pspName->value] ?? null ;
+        if(empty($merchantPsp)) throw new \RuntimeException("PSP not configured");
+        $chargeResult = $merchantPsp->charge($params);
+        $charge = new Charge(
+            id : uniqid('charge_'),
+            merchantId : $merchant->id,
+            amount : $params['amount'],
+            currency : $params['currency'],
+            status : $chargeResult->success ? 'success' : 'failed',
+            transactionId : $chargeResult->transactionId,
+            createdAt : (new \DateTimeImmutable())->format(\DateTime::ATOM),
+        );
+        $this->chargeRepository->save($charge);
+        return $charge;
+    }
+}
